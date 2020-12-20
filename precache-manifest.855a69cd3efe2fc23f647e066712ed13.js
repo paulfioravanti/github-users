c67ca1892d28acc42572b9067ac79b1641355a60
Paul Fioravanti <paul.fioravanti@gmail.com>
2020-12-20 15:57:57 +1100

    Updates

---
+++ precache-manifest.855a69cd3efe2fc23f647e066712ed13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baf8874888fd0a98ff5ec33fa58cb1b5",
+    "url": "/github-users/index.html"
+  },
+  {
+    "revision": "4297a56ad95e167131d3",
+    "url": "/github-users/static/css/main.a4434d93.chunk.css"
+  },
+  {
+    "revision": "58e2c8a50b74d9f552f8",
+    "url": "/github-users/static/js/2.03aa2227.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/github-users/static/js/2.03aa2227.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4297a56ad95e167131d3",
+    "url": "/github-users/static/js/main.605fc1fe.chunk.js"
+  },
+  {
+    "revision": "bcdd95df725045447e28",
+    "url": "/github-users/static/js/runtime-main.1aea2bed.js"
+  }
+]);
